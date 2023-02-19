c8bb1823dc1194b50a2eabd1dcb52b3d2e800d2b
Casaro01 <124566569+Casaro01@users.noreply.github.com>
2023-02-19 16:41:48 +0100

    Nuovo script della cam con rimozione del vecchio per evitare di

--- Assets/Scripts/SwipeController.cs
+++ Assets/Scripts/SwipeController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CamMove1 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class SwipeController : MonoBehaviour
     {
-        
-    }
+    public float minX, maxX, clampY, clampZ;
+    public float swipeSpeed = 0.5f; // Velocità di transizione
+    public float smoothTime = 0.3f; // Tempo di transizione
+    private Vector2 lastPosition; // Ultima posizione toccata
+    private Vector3 velocity = Vector3.zero; // Velocità di transizione
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    void Update() {
+        // Se viene rilevato un tocco
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+
+            // Se il tocco inizia
+            if (touch.phase == TouchPhase.Began) {
+                lastPosition = touch.position;
+                }
+
+            // Se il dito si muove
+            if (touch.phase == TouchPhase.Moved) {
+                // Calcola la differenza di posizione sull'asse X rispetto all'ultimo tocco
+                float deltaX = touch.position.x - lastPosition.x;
+
+                // Calcola la nuova posizione della camera sull'asse X
+                Vector3 newPosition = new Vector3(transform.position.x + deltaX * swipeSpeed, transform.position.y, transform.position.z);
+
+                // Applica la transizione graduale alla nuova posizione
+                transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+
+                lastPosition = touch.position;
+                InBounds();
+                }
+            }
+        }
+    void InBounds() {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), clampY, clampZ);
+            }
+        }
+    
+    
