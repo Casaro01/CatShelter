d83f7137141797a725ea5dfd1edaad27f124de4a
Casaro01 <124566569+Casaro01@users.noreply.github.com>
2023-03-21 15:09:06 +0100

    ripulito script camera
    ripulito script della camera prima di un consulto

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
     {
     public float swipeSpeed = 0.5f; // Velocità di transizione
-    public float smoothTime = 0.3f; // Tempo di transizione
     public float dragSpeed = 0.2f; //velocità di drag
     public Vector2 lastPosition; // Ultima posizione toccata
-    public float moveTowardsSpeed = 0.5f;
+    public float moveTowardsSpeed = 0.5f;//Frame al secondo
     [SerializeField] private Camera cam;
     [SerializeField] private SpriteRenderer bkg;
     float bkgMinX, bkgMaxX;
@@ -33,19 +32,14 @@ public class CameraControl : MonoBehaviour
     public void SwipeCamera(float input) {
         Vector2 enterPosition = new Vector2(input, transform.position.y);
         if (Mathf.Abs(enterPosition.x - lastPosition.x) > 0.1)
-            lastPosition = enterPosition;  
-		Debug.Log("Swiping: " + input);
+            lastPosition = enterPosition;
         float deltaX = (input - lastPosition.x)*swipeSpeed;
-        Debug.Log(deltaX);
         // Calcola la nuova posizione della camera sull'asse X
           newPosition = new Vector3(
             InBounds(transform.position.x - deltaX),
             transform.position.y,
             transform.position.z);
-
         // Applica la transizione graduale alla nuova posizione
-        Debug.Log(transform.position.x - newPosition.x);
-        //if ((transform.position.x - newPosition.x) > 0.1)
         transform.position = Vector3.Lerp(transform.position, newPosition, moveTowardsSpeed*Time.deltaTime);
         //si salva l'ultima per il deltaX all'inizio
         lastPosition = enterPosition;
@@ -57,12 +51,12 @@ public class CameraControl : MonoBehaviour
             //drag verso sinistra
             Vector3 newPosition = new Vector3(InBounds(transform.position.x + Vector3.left.x * dragSpeed), transform.position.y, transform.position.z);
             // Applica la transizione graduale alla nuova posizione
-            transform.position = Vector3.Lerp(transform.position, newPosition, smoothTime);
+            transform.position = Vector3.Lerp(transform.position, newPosition, moveTowardsSpeed*Time.deltaTime);
 
             } else if (viewPoint.x >= 0.8) {
                 Vector3 newPosition = new Vector3(InBounds(transform.position.x + Vector3.right.x * dragSpeed), transform.position.y, transform.position.z);
                  // Applica la transizione graduale alla nuova posizione
-                 transform.position = Vector3.Lerp(transform.position, newPosition, smoothTime);
+                 transform.position = Vector3.Lerp(transform.position, newPosition, moveTowardsSpeed * Time.deltaTime);
             }
     }
         }
