9d0b664591b24a7cc57926e4e1e1cb5a205099e6
Casaro01 <124566569+Casaro01@users.noreply.github.com>
2023-05-02 16:52:52 +0200

    WIP UI

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIButton : MonoBehaviour
+{
+    public GameObject sonPanel;
+    UnityEvent GotPressed;
+
+
+    public void OpenSon() {
+
+        sonPanel.SetActive(true);
+        }
+    onC
+}
